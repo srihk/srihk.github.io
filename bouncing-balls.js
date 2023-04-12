99a2a9bc141859e5ccb7b4fc22ac2941403dce46
Sriharsha Kotcharlakot <k.venkatsriharsha@gmail.com>
2023-04-12 23:36:53 +0530

    Add mini-game where the player can collect the balls with keyboard input (wasd).

--- bouncing-balls.js
+++ bouncing-balls.js
@@ -21,14 +21,21 @@ function randomRGB() {
     return `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
 }
 
-class Ball {
-    constructor(x, y, velX, velY, color, size) {
+class Shape {
+    constructor(x, y, velX, velY) {
         this.x = x;
         this.y = y;
         this.velX = velX;
         this.velY = velY;
+    }
+}
+
+class Ball extends Shape {
+    constructor(x, y, velX, velY, color, size) {
+        super(x, y, velX, velY);
         this.color = color;
         this.size = size;
+        this.exists = true;
     }
 
     draw() {
@@ -61,7 +68,7 @@ class Ball {
 
     collisionDetect() {
         for (const ball of balls) {
-            if (this !== ball) {
+            if (this !== ball && ball.exists) {
                 const dx = this.x - ball.x;
                 const dy = this.y - ball.y;
                 const distance = Math.sqrt(dx * dx + dy * dy);
@@ -77,6 +84,126 @@ class Ball {
                 }
             }
         }
+    }
+}
+
+class Player extends Shape {
+    static State = {
+        DISABLED: "disabled",
+        ENABLED: "enabled",
+        RELEASING: "releasing"
+    }
+    
+    constructor(x, y) {
+        super(x, y, 10, 10);
+        this.color = `rgb(255, 0, 0)`;
+        this.size = 20;
+        this.state = Player.State.DISABLED;
+        this.balls = [];
+
+        window.addEventListener("keydown", (event) => {
+            if (this.state === Player.State.DISABLED) {
+                if (["w", "a", "s", "d"].includes(event.key)) {
+                    this.state = Player.State.ENABLED;
+                } else {
+                    return;
+                }
+            }
+
+            switch(event.key) {
+                case "a":
+                    this.x -= this.velX;
+                    break;
+                case "d":
+                    this.x += this.velX;
+                    break;
+                case "w":
+                    this.y -= this.velY;
+                    break;
+                case "s":
+                    this.y += this.velY;
+                    break;
+                case "Escape":
+                    if (this.state === Player.State.ENABLED) {
+                        this.state = Player.State.RELEASING;
+                        this.releaseBalls();
+                    }
+                    break;
+            }
+        });
+    }
+
+    draw() {
+        ctx.beginPath();
+        let alpha = 0.8;
+        if (this.state === Player.State.DISABLED) {
+            alpha = 0.2;
+            this.x = width - 50;
+            this.y = height - 50;
+        }
+
+        const color = `rgba(${this.color.replace(/[^\d,]/g, " ")}, ${alpha})`;
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.font = "20px JetBrains Mono";
+        ctx.fillStyle = color;
+        if (this.state === Player.State.DISABLED) {
+            ctx.fillText("wa", this.x - (this.size / 5) * 3, this.y - 2);
+            ctx.fillText("sd", this.x - (this.size / 5) * 3, this.y + this.size / 1.6);
+        } else {
+            ctx.fillText(`${this.balls.length}`.padStart(2, "0"), this.x - (this.size / 5) * 3, this.y + this.size / 3);
+        }
+    }
+
+    checkBounds() {
+        if (this.x + this.size >= width) {
+            this.x = width - this.size;
+        }
+
+        if (this.x - this.size <= 0) {
+            this.x = this.size;
+        }
+
+        if (this.y + this.size >= height) {
+            this.y = height - this.size;
+        }
+
+        if (this.y - this.size <= 0) {
+            this.y = this.size;
+        }
+    }
+
+    collisionDetect() {
+        if (this.state !== Player.State.ENABLED) {
+            return;
+        }
+        
+        for (const ball of balls) {
+            if (ball.exists) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + ball.size) {
+                    ball.exists = false;
+                    this.balls.push(ball);
+                }
+            }
+        }
+    }
+
+    releaseBalls() {
+        if (this.balls.length === 0) {
+            this.state = Player.State.DISABLED;
+            return;
+        }
+
+        const ball = this.balls.pop();
+        ball.exists = true;
+        ball.x = random(this.size, width - this.size);
+        ball.y = random(this.size, height - this.size);
     }
 }
 
@@ -101,12 +228,24 @@ function loop() {
     ctx.fillRect(0, 0, width, height);
 
     for (const ball of balls) {
-        ball.draw();
-        ball.update();
-        ball.collisionDetect();
+        if (ball.exists) {
+            ball.draw();
+            ball.update();
+            ball.collisionDetect();
+        }
+    }
+
+    player.draw();
+    player.checkBounds();
+    player.collisionDetect();
+
+    if (player.state === Player.State.RELEASING) {
+        player.releaseBalls();
     }
 
     requestAnimationFrame(loop);
 }
+
+const player = new Player(width - 50, height - 50);
 
 loop();
